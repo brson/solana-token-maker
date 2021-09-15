fd92371f73cf28f224b1a3067a0bb6d337773ba5
Brian Anderson <andersrb@gmail.com>
2021-09-14 23:43:01 -0600

    .

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 console.assert(solanaWeb3);
 console.assert(splToken);
 
-let web3 = solanaWeb3;
+const web3 = solanaWeb3;
 
-console.log(web3);
-console.log(splToken);
+// For console testing
+document.web3 = web3;
+document.splToken = splToken;
 
 const {
     Token
 } = splToken;
 
-console.log(Token);
-
-let connection = new web3.Connection(
+const connection = new web3.Connection(
     web3.clusterApiUrl('devnet'),
     'confirmed',
 );
+
+const walletPrivkeyInput = document.getElementById("wallet-privkey");
+const walletPubkeySpan = document.getElementById("wallet-pubkey");
+const walletLamportsSpan = document.getElementById("wallet-lamports");
+
+console.assert(walletPrivkeyInput);
+console.assert(walletPubkeySpan);
+console.assert(walletLamportsSpan);
+
+// https://www.thetopsites.net/article/50868276.shtml
+const fromHexString = hexString =>
+  new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+
+const toHexString = bytes =>
+  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+
+let keypair = new web3.Keypair();
+
+let secretKeyHex = toHexString(keypair.secretKey);
+let publicKeyHex = keypair.publicKey.toString();
+
+walletPrivkeyInput.value = secretKeyHex;
+walletPubkeySpan.innerText = publicKeyHex;
+
+walletPrivkeyInput.addEventListener("input", (e) => {
+    walletPubkeySpan.innerText = "";
+    let maybePrivKey = walletPrivkeyInput.value;
+
+    // todo
+});
+
