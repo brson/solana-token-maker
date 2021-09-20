95cffdfc7930552348648ad6533b4771ce2b7c66
Brian Anderson <andersrb@gmail.com>
2021-09-19 23:48:07 -0600

    .

--- programs/blob2/src/lib.rs
+++ programs/blob2/src/lib.rs
@@ -18,7 +18,38 @@ pub mod blob2 {
     use super::*;
 
     pub fn init(ctx: Context<Init>) -> ProgramResult {
-        todo!()
+        let initial_storage = Pubkey::find_program_address(&[b"init"], ctx.program_id);
+        let (initial_storage, initial_storage_bump_seed) = initial_storage;
+        assert_eq!(&initial_storage, ctx.accounts.initial_storage.key);
+
+        {
+            let from = ctx.accounts.payer.key;
+            let to = ctx.accounts.initial_storage.key;
+            let space = 2;
+            let lamports = 10000;
+            let owner = ctx.program_id;
+
+            invoke_signed(
+                &system_instruction::create_account(
+                    from, to, space, lamports, owner
+                ),
+                &[
+                    ctx.accounts.payer.clone(),
+                    ctx.accounts.initial_storage.clone(),
+                    ctx.accounts.system_program.clone(),
+                ],
+                &[
+                    &[
+                        b"init",
+                        &[initial_storage_bump_seed]
+                    ]
+                ],
+            )?;
+        }
+
+        ctx.accounts.storage_reference.storage = initial_storage;
+
+        Ok(())
     }
 
     pub fn set(
@@ -37,14 +68,18 @@ const INITIALIZED: u8 = 0xAE;
 pub struct Init<'info> {
     #[account(mut, signer)]
     pub payer: AccountInfo<'info>,
+    #[account(signer)]
+    pub base: AccountInfo<'info>,
     #[account(zero)]
     pub storage_reference: ProgramAccount<'info, StorageReference>,
     #[account(
         mut,
         constraint = initial_storage.owner == &system_program::ID,
-        constraint = initial_storage.data.borrow().is_empty()
+        constraint = initial_storage.data.borrow().is_empty(),
     )]
     pub initial_storage: AccountInfo<'info>,
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
