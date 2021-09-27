1b0d6453db34fe7eb8c44b0bc9a1645b80929364
Brian Anderson <andersrb@gmail.com>
2021-09-26 20:13:02 -0600

    .

--- app/blob2.js
+++ app/blob2.js
@@ -6,38 +6,118 @@ const anchor = nodeBundle.anchor;
 const blob2Idl = await fetch("../target/idl/blob2.json").then(r => r.json());
 const blob2PrgramIdDevnet = "J6h7RJ9EwLxvEYLqDDvtF1S8GxijChuy5zRKy1NNpU1P";
 
-async function create(anchorProvider) {
+export async function create(anchorProvider) {
     const blob2 = new anchor.Program(blob2Idl, blob2PrgramIdDevnet, anchorProvider);
     return blob2;
 }
 
-async function readKeyJson(blob2, payer, base, key) {
+export async function readKeyJson(blob2, payer, base, key) {
 }
 
-async function writeKeyJson(blob2, payer, base, key, value) {
+export async function writeKeyJson(blob2, payer, base, key, value) {
 }
 
-async function readKey(blob2, payer, base, key) {
+export async function readKeyString(blob2, payer, base, key) {
+}
+
+export async function writeKeyString(blob2, payer, base, key, value) {
+}
+
+export async function readKeyBytes(blob2, payer, base, key) {
     let provider = blob2.provider;
-    const [ tokensStorageReference, tokensStorageReferenceBumpSeed ] = await tokensStorageReference();
+    const [ storageReference, storageReferenceBumpSeed ] = await storageReferenceForKey(base, key);
 
-    let storage = await getStorage(anchorProvider, blob2, tokensStorageReference);
+    let storage = await getStorage(anchorProvider, blob2, storageReference);
+
+    return storage;
 }
 
-async function writeKey(blob2, payer, base, key, value) {
+export async function writeKeyBytes(blob2, payer, base, key, value) {
+    let provider = blob2.provider;
+    const [ storageReference, storageReferenceBumpSeed ] = await storageReferenceForKey(base, key);
+
+    {
+        let storageReferenceAccountInfo = await provider.connection.getAccountInfo(storageReference);
+        let storageReferenceData = storageReferenceAccountInfo.data;
+
+        if (storageReferenceData.length == 0) {
+            await initStorage(blob2, payer, base, key);
+        }
+    }
+
+    let storageReferenceValue = await blob2.account.storageReference.fetch(storageReference);
+    let storage = storageReferenceValue.storage;
+    
+    const [ nextStorage, nextStorageBumpSeed ]  = await anchor.web3.PublicKey.findProgramAddress(
+        [
+            "next",
+            initialStorage.toBuffer()
+        ],
+        blob.programId
+    );
+
+    console.assert(typeof value != "string");
+
+    await blob.rpc.set(value, {
+        accounts: {
+            payer: payer.publicKey,
+            storageReference: storageReference,
+            storage: initialStorage,
+            nextStorage: nextStorage,
+            systemProgram: anchor.web3.SystemProgram.programId
+        },
+        signers: [
+            payer
+        ],
+    });
+}
+
+async function initStorage(blob2, payer, base, key) {
+    let provider = blob2.provider;
+    const [ storageReference, storageReferenceBumpSeed ] = await storageReferenceForKey(base, key);
+
+    const [ initialStorage, initialStorageBumpSeed ]  = await anchor.web3.PublicKey.findProgramAddress(
+        [
+            "init",
+            storageReference.toBuffer()
+        ],
+        blob2.programId
+    );
+    
+    await blob.rpc.init(Buffer.from(key), storageReferenceBumpSeed, {
+        accounts: {
+            payer: payer.publicKey,
+            storageReference: storageReference,
+            initialStorage: initialStorage,
+            systemProgram: anchor.web3.SystemProgram.programId
+        },
+        signers: [
+            payer
+        ],
+    });
 }
 
 async function getStorage(blob2, storageReference) {
     let provider = blob2.provider;
+
+    {
+        let storageReferenceAccountInfo = await provider.connection.getAccountInfo(storageReference);
+        let storageReferenceData = storageReferenceAccountInfo.data;
+
+        if (storageReferenceData.length == 0) {
+            return null;
+        }
+    }
+
     let storageReferenceValue = await blob2.account.storageReference.fetch(storageReference);
     let storage = storageReferenceValue.storage;
 
     let storageAccountInfo = await provider.connection.getAccountInfo(storage);
-    let data = storageAccountInfo.data;
+    let storageData = storageAccountInfo.data;
 
-    assert.ok(data.length > 0);
-    let header = data[0];
-    let payload = data.slice(1);
+    assert.ok(storageData.length > 0);
+    let header = storageData[0];
+    let payload = storageData.slice(1);
     if (header == 0) {
         return null;
     } else {
@@ -45,15 +125,15 @@ async function getStorage(blob2, storageReference) {
     }
 }
 
-async function tokensStorageReference(base) {
-    const [ tokensStorageReference, tokensStorageReferenceBumpSeed ] = await anchor.web3.PublicKey.findProgramAddress(
+async function storageReferenceForKey(base, key) {
+    const [ storageReference, storageReferenceBumpSeed ] = await anchor.web3.PublicKey.findProgramAddress(
         [
             "key",
             base.publicKey.toBuffer(),
-            "tokens",
+            key,
         ],
         blob2.programId
     );
 
-    return [ tokensStorageReference, tokensStorageReferenceBumpSeed ];
+    return [ storageReference, storageReferenceBumpSeed ];
 }
